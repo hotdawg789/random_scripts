a1d7d01bf2bd8aebf449d44d50e3cd58cd154bad
how dawg <hotdawg789@gmail.com>
2023-12-11 17:27:40 -0500

    jenkins

--- jenkins/printlabels.groovy
+++ jenkins/printlabels.groovy
@@ -1,13 +1,13 @@
-import hudson.model.Job
+import hudson.model.Queue
+import hudson.model.queue.QueueTaskFuture
 import com.cloudbees.hudson.plugins.folder.Folder
 import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import java.util.Calendar
 
 // List of node labels you are looking for
 def nodeLabels = ['label1', 'label2', 'label3'] // Replace with your labels
 
-// Function to check if a job is using any of the specified node labels
-def usesAnyNodeLabel(Job job, List nodeLabels) {
+// Function to check if a job is intended to use any of the specified node labels
+def isIntendedForAnyNodeLabel(Job job, List nodeLabels) {
     def assignedLabel = null
 
     if (job instanceof WorkflowJob) {
@@ -28,29 +28,16 @@ def usesAnyNodeLabel(Job job, List nodeLabels) {
     return false
 }
 
-// Function to check if the last build was within the last 6 months
-def wasBuiltInLastSixMonths(Job job) {
-    def lastBuild = job.getLastBuild()
-    if (lastBuild == null) {
-        return false
-    }
-    def sixMonthsAgo = Calendar.instance
-    sixMonthsAgo.add(Calendar.MONTH, -6)
-    return lastBuild.getTimeInMillis() > sixMonthsAgo.timeInMillis
-}
+// Get the build queue
+Queue queue = Jenkins.instance.queue
 
-// Iterate over all items in Jenkins and check for the node labels and last build date
-Jenkins.instance.allItems.each { item ->
-    // Check for all types of jobs including those inside folders
-    if (item instanceof Folder) {
-        item.getAllJobs().each {
-            if (usesAnyNodeLabel(it, nodeLabels) && wasBuiltInLastSixMonths(it)) {
-                println("${it.fullName} uses one of the labels: ${nodeLabels.join(', ')} and was built in the last 6 months")
-            }
-        }
-    } else if (item instanceof Job) {
-        if (usesAnyNodeLabel(item, nodeLabels) && wasBuiltInLastSixMonths(item)) {
-            println("${item.fullName} uses one of the labels: ${nodeLabels.join(', ')} and was built in the last 6 months")
+// Iterate over all items in the build queue
+queue.items.each { queueItem ->
+    def task = queueItem.task
+    if (task instanceof Job) {
+        Job job = (Job) task
+        if (isIntendedForAnyNodeLabel(job, nodeLabels)) {
+            println("${job.fullName} is in the build queue and is intended for one of the labels: ${nodeLabels.join(', ')}")
         }
     }
 }
