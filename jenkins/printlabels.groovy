743e6b0ca48493218b45c116e7d8f25a75054757
how dawg <hotdawg789@gmail.com>
2023-12-11 17:21:41 -0500

    jenkins

--- jenkins/printlabels.groovy
+++ jenkins/printlabels.groovy
@@ -1,6 +1,7 @@
 import hudson.model.Job
 import com.cloudbees.hudson.plugins.folder.Folder
 import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import java.util.Calendar
 
 // List of node labels you are looking for
 def nodeLabels = ['label1', 'label2', 'label3'] // Replace with your labels
@@ -27,18 +28,29 @@ def usesAnyNodeLabel(Job job, List nodeLabels) {
     return false
 }
 
-// Iterate over all items in Jenkins and check for the node labels
+// Function to check if the last build was within the last 6 months
+def wasBuiltInLastSixMonths(Job job) {
+    def lastBuild = job.getLastBuild()
+    if (lastBuild == null) {
+        return false
+    }
+    def sixMonthsAgo = Calendar.instance
+    sixMonthsAgo.add(Calendar.MONTH, -6)
+    return lastBuild.getTimeInMillis() > sixMonthsAgo.timeInMillis
+}
+
+// Iterate over all items in Jenkins and check for the node labels and last build date
 Jenkins.instance.allItems.each { item ->
     // Check for all types of jobs including those inside folders
     if (item instanceof Folder) {
         item.getAllJobs().each {
-            if (usesAnyNodeLabel(it, nodeLabels)) {
-                println("${it.fullName} uses one of the labels: ${nodeLabels.join(', ')}")
+            if (usesAnyNodeLabel(it, nodeLabels) && wasBuiltInLastSixMonths(it)) {
+                println("${it.fullName} uses one of the labels: ${nodeLabels.join(', ')} and was built in the last 6 months")
             }
         }
     } else if (item instanceof Job) {
-        if (usesAnyNodeLabel(item, nodeLabels)) {
-            println("${item.fullName} uses one of the labels: ${nodeLabels.join(', ')}")
+        if (usesAnyNodeLabel(item, nodeLabels) && wasBuiltInLastSixMonths(item)) {
+            println("${item.fullName} uses one of the labels: ${nodeLabels.join(', ')} and was built in the last 6 months")
         }
     }
 }
