ebf815e9f857aa20382e4ac25708daf29857bee9
how dawg <hotdawg789@gmail.com>
2024-05-23 10:49:28 -0400

    jenkins executors

--- jenkins/executors_longer_than_24.groovy
+++ jenkins/executors_longer_than_24.groovy
@@ -12,47 +12,6 @@ def printJobDetails(job, build) {
     println "Job: ${job.fullDisplayName}, Build: #${build.number}, Duration: ${duration / 1000 / 60 / 60} hours"
 }
 
-// Iterate over all executors
-Jenkins.instance.computers.each { computer ->
-    computer.executors.each { executor ->
-        def executable = executor.currentExecutable
-        if (executable != null) {
-            def build = executable.getParent()
-            def job = build.getParent()
-            def duration = now - build.getStartTimeInMillis()
-
-            if (duration > longRunningThreshold) {
-                printJobDetails(job, build)
-            }
-        }
-    }
-
-    computer.oneOffExecutors.each { executor ->
-        def executable = executor.currentExecutable
-        if (executable != null) {
-            def build = executable.getParent()
-            def job = build.getParent()
-            def duration = now - build.getStartTimeInMillis()
-
-            if (duration > longRunningThreshold) {
-                printJobDetails(job, build)
-            }
-        }
-    }
-}import jenkins.model.*
-import hudson.model.*
-import java.util.concurrent.TimeUnit
-
-// Threshold for long-running jobs (24 hours in milliseconds)
-def longRunningThreshold = TimeUnit.HOURS.toMillis(24)
-def now = System.currentTimeMillis()
-
-// Function to print job details
-def printJobDetails(job, build) {
-    def duration = now - build.getStartTimeInMillis()
-    println "Job: ${job.fullDisplayName}, Build: #${build.number}, Duration: ${duration / 1000 / 60 / 60} hours"
-}
-
 // Function to get the start time of different types of builds
 def getBuildStartTime(build) {
     if (build instanceof Run) {
@@ -98,8 +57,5 @@ Jenkins.instance.computers.each { computer ->
         }
     }
 }
-
-println "Finished checking for long-running builds."
-
 
 println "Finished checking for long-running builds."
