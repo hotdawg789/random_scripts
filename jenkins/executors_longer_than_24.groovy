b581e769e5d0f5b4ca0f9d9c21ce0dc9dc1e13bd
how dawg <hotdawg789@gmail.com>
2024-05-23 11:23:06 -0400

    jenkins executors

--- jenkins/executors_longer_than_24.groovy
+++ jenkins/executors_longer_than_24.groovy
@@ -1,6 +1,7 @@
 import jenkins.model.*
 import hudson.model.*
 import java.util.concurrent.TimeUnit
+import org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution.PlaceholderTask
 
 // Threshold for long-running jobs (24 hours in milliseconds)
 def longRunningThreshold = TimeUnit.HOURS.toMillis(24)
@@ -22,40 +23,64 @@ def getBuildStartTime(build) {
     }
 }
 
+// Function to handle PlaceholderExecutable
+def handlePlaceholderExecutable(executable) {
+    def parentRun = executable.parentExecutable
+    if (parentRun != null && parentRun instanceof Run) {
+        def build = parentRun
+        if (build.isBuilding()) {
+            def job = build.getParent()
+            def startTime = getBuildStartTime(build)
+            if (startTime > 0) {
+                def duration = now - startTime
+                if (duration > longRunningThreshold) {
+                    printJobDetails(job, build, duration)
+                }
+            }
+        }
+    }
+}
+
 // Iterate over all executors
 Jenkins.instance.computers.each { computer ->
     computer.executors.each { executor ->
         def executable = executor.currentExecutable
-        if (executable != null && executable instanceof Run) {
-            def build = executable
-            if (build.isBuilding()) {  // Check if the build is still running
-                def job = build.getParent()
-                def startTime = getBuildStartTime(build)
-
-                if (startTime > 0) {
-                    def duration = now - startTime
-                    if (duration > longRunningThreshold) {
-                        printJobDetails(job, build, duration)
+        if (executable != null) {
+            if (executable instanceof Run) {
+                def build = executable
+                if (build.isBuilding()) {  // Check if the build is still running
+                    def job = build.getParent()
+                    def startTime = getBuildStartTime(build)
+                    if (startTime > 0) {
+                        def duration = now - startTime
+                        if (duration > longRunningThreshold) {
+                            printJobDetails(job, build, duration)
+                        }
                     }
                 }
+            } else if (executable instanceof PlaceholderTask.PlaceholderExecutable) {
+                handlePlaceholderExecutable(executable)
             }
         }
     }
 
     computer.oneOffExecutors.each { executor ->
         def executable = executor.currentExecutable
-        if (executable != null && executable instanceof Run) {
-            def build = executable
-            if (build.isBuilding()) {  // Check if the build is still running
-                def job = build.getParent()
-                def startTime = getBuildStartTime(build)
-
-                if (startTime > 0) {
-                    def duration = now - startTime
-                    if (duration > longRunningThreshold) {
-                        printJobDetails(job, build, duration)
+        if (executable != null) {
+            if (executable instanceof Run) {
+                def build = executable
+                if (build.isBuilding()) {  // Check if the build is still running
+                    def job = build.getParent()
+                    def startTime = getBuildStartTime(build)
+                    if (startTime > 0) {
+                        def duration = now - startTime
+                        if (duration > longRunningThreshold) {
+                            printJobDetails(job, build, duration)
+                        }
                     }
                 }
+            } else if (executable instanceof PlaceholderTask.PlaceholderExecutable) {
+                handlePlaceholderExecutable(executable)
             }
         }
     }
